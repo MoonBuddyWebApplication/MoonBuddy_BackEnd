b372b6a608dec33bf7346d0429f1f5c28c692794
chae401 <chae401@sju.ac.kr>
2023-07-06 18:32:16 +0900

    init

---
+++ moonbuddy/src/main/java/com/project/moonbuddy/MoonbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.project.moonbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonbuddyApplication.class, args);
+	}
+
+}
